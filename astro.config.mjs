643f429101dbcfb685f5c79044c3fd14b537e072
masam <dummy@example.com>
2025-05-11 22:39:27 +0900

    fix: tailwindcss config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
+
+import tailwindcss from '@tailwindcss/vite';
 
 const BASE_PATH = '/';
 
 export default defineConfig({
-	site: 'https://compQx7.github.io',
-	base: BASE_PATH,
-	output: 'static', // hybrid
-	integrations: [
-		react(),
-		sitemap({}),
+  site: 'https://compQx7.github.io',
+  base: BASE_PATH,
+
+  output: 'static',
+
+  integrations: [
+      react(),
+      sitemap({}),
 	],
-	vite: {
-		plugins: [
-			tailwindcss(),
-		],
-	},
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
