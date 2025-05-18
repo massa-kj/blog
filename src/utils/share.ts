4a4673d3d0d80a9e4a039dc719b8959f49885b9b
masam <dummy@example.com>
2025-05-18 16:49:11 +0900

    refactor: share menu values etc...

--- src/utils/share.ts
+++ src/utils/share.ts
@@ -1,10 +1,40 @@
 export const BASE_URL = import.meta.env.BASE_URL;
 
-export const LABEL_MAP = {
-  'blog': 'Home',
-  'articles': 'Articles',
-  'cheatsheets': 'Cheat Sheets',
-  'about': 'About',
-  '404': '404: Page not found',
+export const TOP_MENU = {
+	articles: 'articles',
+	cheatsheets: 'cheatsheets',
+	tags: 'tags',
+	about: 'about',
 };
+
+export const BREADCRUMBS_DEFINITION = {
+  'blog': 'Home',
+  '404': '404: Page not found',
+  [TOP_MENU.articles]: 'Articles',
+  [TOP_MENU.cheatsheets]: 'Cheat Sheets',
+  [TOP_MENU.about]: 'About',
+};
+
+export const MENU_DEFINITION = [
+  {
+    label: 'Articles',
+    path: `/${TOP_MENU.articles}`,
+    children: [
+      { label: '2025', path: `/${TOP_MENU.articles}/2025` },
+      { label: '2024', path: `/${TOP_MENU.articles}/2024` },
+    ],
+  },
+  {
+    label: 'Cheat Sheets',
+    path: `/${TOP_MENU.cheatsheets}`,
+  },
+  {
+    label: 'Tags',
+    path: `/${TOP_MENU.tags}`,
+  },
+  {
+    label: 'About',
+    path: `/${TOP_MENU.about}`,
+  },
+];
 
