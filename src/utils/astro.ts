3d22ef5d8c931d17cedf46ac80d0f2d4c14785e6
masam <dummy@example.com>
2025-05-19 15:56:26 +0900

    refactor: organize layout styles

--- src/utils/astro.ts
+++ src/utils/astro.ts
@@ -1,8 +1,10 @@
 export function slugify(str: string): string {
   return str
+    .trim()
     .toLowerCase()
     .replace(/\s+/g, '-')       // space → hyphen
     .replace(/[^\w-]/g, '');    // remove non-word characters
+    // .replace(/[^\w\-ぁ-んァ-ヶ一-龠]/g, '') // Japanese
 }
 
 export function generateToc(data: { depth: number, slug: string, text: string }): Array<{ level: number, id: string, text: string }> {
