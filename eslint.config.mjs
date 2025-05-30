5005eb9151cc5c5db4e740cbdccb04977cab91fb
masam <compQx7@users.noreply.github.com>
2025-05-30 09:06:35 +0900

    style: Update ESLint config to use a shared rules object and improve ignores.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,24 +2,38 @@ import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import astro from 'eslint-plugin-astro';
 import react from 'eslint-plugin-react';
-import markdown from 'eslint-plugin-markdown';
+// import markdown from 'eslint-plugin-markdown';
+
+const commonRules = {
+  // '@typescript-eslint/explicit-module-boundary-types': 'off',
+  // '@typescript-eslint/no-explicit-any': 'warn',
+
+  // '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+  // '@typescript-eslint/no-var-requires': 'error',
+  'no-console': ['warn', { allow: ['warn', 'error'] }],
+  'no-debugger': 'warn',
+
+  'semi': ['error', 'always'],
+  'quotes': ['error', 'single', { avoidEscape: true }],
+  'comma-dangle': ['error', 'always-multiline'],
+
+  // 'unused-imports/no-unused-imports': 'warn',
+  // 'unused-imports/no-unused-vars': [
+  //   'warn',
+  //   { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+  // ],
+};
 
 export default [
   js.configs.recommended,
   ...astro.configs.recommended,
 
   {
-    ignores: ["eslint.config.mjs", ".astro/", "dist/", "tsconfig.json"] // acts as global ignores, due to the absence of other properties
+    ignores: ['.astro/', 'dist/', 'tsconfig.json'], // acts as global ignores, due to the absence of other properties
   },
 
   {
-    rules: {
-      'semi': ['error', 'always'],
-    },
-  },
-
-  {
-    files: ['**/*.ts', '**/*.tsx'],
+    files: ['**/*.mjs', '**/*.ts', '**/*.tsx'],
     languageOptions: {
       parser: tseslint.parser,
       parserOptions: {
@@ -27,6 +41,7 @@ export default [
       },
     },
     rules: {
+      ...commonRules,
     },
   },
 
@@ -44,19 +59,25 @@ export default [
     },
     rules: {
       ...react.configs.recommended.rules,
+      ...commonRules,
     },
   },
 
   {
     files: ['**/*.astro'],
+    languageOptions: {
+      parser: astro.parser,
+      parserOptions: {
+        extraFileExtensions: ['.astro'],
+      },
+    },
+    plugins: {
+      astro,
+    },
     rules: {
+      ...astro.configs.recommended.rules,
+      ...commonRules,
     },
   },
-
-  // {
-  //   files: ['**/*.md'],
-  //   plugins: { markdown },
-  //   processor: 'markdown/markdown',
-  // },
 ];
 
